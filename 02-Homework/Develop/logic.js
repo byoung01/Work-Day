eff2de5099bd5f761cf2c7bc383dde8fc917d5b8
young <brevinyoung@gmail.com>
2021-10-17 18:51:53 -0700

    background changes color depending on time

--- 02-Homework/Develop/logic.js
+++ 02-Homework/Develop/logic.js
@@ -2,49 +2,37 @@ var activities = document.querySelector("textarea");
 var data = JSON.parse(localStorage.getItem("day-schedule"));
 var headerTime = document.querySelector("h2");
 
-//Psudo code base
+function formatDate() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  let hour = date.getHours();
+  if (hour > 12) {
+    hour -= 12;
+  }
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  return (today = `${month}/${day}/${year} ${hour}:${minutes}:${seconds}`);
+}
 
-//onload if any local data
-// on page load want to use var data to retrieve the locally saved data
-//if null dont display in text box
+$(".time-block").each(function (data) {
+  // getting each blocks set time by taking in data and adding 9
+  const time = (data += 9);
 
-//text
-//get an inpot from the user
-//store user input locally when save button pressed
+  // getting current hour
+  const date = new Date();
+  const hour = date.getHours();
 
-//color
-//get users time
-//if time is past current hour make bg gray
-//if time is users current time make bg red
-//else make bg green
-
-//time
-//get users current time and display it in header
-//use users current time to
-
-// function formatDate(today) {
-//   var year = today.getFullYear(),
-//     month = today.getMonth() + 1,
-//     date = today.getDate(),
-//     hour = today.getHours(),
-//     minutes = today.getMinutes(),
-//     seconds = today.getSeconds();
-//   return `${month}/${date}/${year} ${hour}:${minutes}:${seconds}`;
-// }
-pageLoad();
-$(".time-block").each(function (Date) {
-  // const date = new Date();
-  // // const hour = new.getHours();
-  // debugger;
-  var id = $(this).attr("id");
-  var id = parseInt(id.split("-").pop());
-  console.log(id);
-  // if (id === date.getHours()) {
-  //   $("description").style.color = "red";
-  //   console.log(id);
-  // } else (id > date.getHours()) {
-  //   $("description").style.color = "green";
-  // }
+  if (hour == time) {
+    //if hour and time are the same
+    $("textarea").addClass("bg-info");
+  } else if (time < hour) {
+    //if time is less than hours
+    $("textarea").addClass("bg-secondary");
+  } else {
+    $("textarea").addClass("bg-warning");
+  }
 });
 function pageLoad() {
   activities.append(data);
@@ -52,15 +40,12 @@ function pageLoad() {
 
 $(".saveBtn").on("click", function (event) {
   event.preventDefault();
-  var val = $(this).siblings("textarea").eq(0).val().trim();
-  var id = $(this).parent().attr("id");
-  var id = parseInt(id.split("-").pop());
-  data = val;
-  // console.log(userText);
+
+  console.log(userText);
   localStorage.setItem("day-schedule", JSON.stringify(data));
 });
+
 // setInterval(function () {
-//   DocumentFragment.getElementById("currentDay").innerHTML = formatDate(
-//     new Date()
-//   );
+//   formatDate();
+//   $("#currentDay").text(formatDate);
 // }, 1000);
